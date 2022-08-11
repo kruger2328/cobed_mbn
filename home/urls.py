2335938e6fe9c486a79f0a6b6d0c5a5a8e019c8f
Mubin-Hardy <mubinmattel@gmail.com>
2022-08-12 01:07:16 +0530

    dfsd

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+app_name = 'home'
 urlpatterns=[
 
     path('home_view_patients',views.view_patients,name='home_view_patients'),
